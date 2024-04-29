4442cde639e801b37caf55777192306ba37a2f5b
Feketebalazs <balazs.torres@gmail.com>
2024-04-29 18:08:07 +0200

    ghf

--- ApiSample/Form1.Designer.cs
+++ ApiSample/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.textBox_kereses = new System.Windows.Forms.TextBox();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.textBox_mennyiseg = new System.Windows.Forms.TextBox();
@@ -37,20 +38,25 @@
             this.button_minus = new System.Windows.Forms.Button();
             this.button_megse = new System.Windows.Forms.Button();
             this.button_mentes = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // textBox_kereses
             // 
-            this.textBox_kereses.Location = new System.Drawing.Point(45, 29);
+            this.textBox_kereses.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(113)))), ((int)(((byte)(131)))), ((int)(((byte)(85)))));
+            this.textBox_kereses.Location = new System.Drawing.Point(12, 12);
             this.textBox_kereses.Name = "textBox_kereses";
             this.textBox_kereses.Size = new System.Drawing.Size(217, 20);
             this.textBox_kereses.TabIndex = 0;
             this.textBox_kereses.TextChanged += new System.EventHandler(this.textBox_kereses_TextChanged_1);
+//            this.textBox_kereses.Leave += new System.EventHandler(this.textBox_kereses_Leave);
             // 
             // listBox1
             // 
+            this.listBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(113)))), ((int)(((byte)(131)))), ((int)(((byte)(85)))));
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(45, 92);
+            this.listBox1.Location = new System.Drawing.Point(12, 55);
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(217, 316);
             this.listBox1.TabIndex = 1;
@@ -58,7 +64,8 @@
             // 
             // textBox_mennyiseg
             // 
-            this.textBox_mennyiseg.Location = new System.Drawing.Point(426, 151);
+            this.textBox_mennyiseg.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(113)))), ((int)(((byte)(131)))), ((int)(((byte)(85)))));
+            this.textBox_mennyiseg.Location = new System.Drawing.Point(306, 229);
             this.textBox_mennyiseg.Name = "textBox_mennyiseg";
             this.textBox_mennyiseg.Size = new System.Drawing.Size(100, 20);
             this.textBox_mennyiseg.TabIndex = 2;
@@ -66,7 +73,7 @@
             // label_1
             // 
             this.label_1.AutoSize = true;
-            this.label_1.Location = new System.Drawing.Point(374, 154);
+            this.label_1.Location = new System.Drawing.Point(254, 232);
             this.label_1.Name = "label_1";
             this.label_1.Size = new System.Drawing.Size(46, 13);
             this.label_1.TabIndex = 3;
@@ -75,7 +82,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(531, 154);
+            this.label2.Location = new System.Drawing.Point(411, 232);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(108, 13);
@@ -84,7 +91,7 @@
             // 
             // button_plus
             // 
-            this.button_plus.Location = new System.Drawing.Point(668, 123);
+            this.button_plus.Location = new System.Drawing.Point(524, 198);
             this.button_plus.Name = "button_plus";
             this.button_plus.Size = new System.Drawing.Size(44, 44);
             this.button_plus.TabIndex = 10;
@@ -94,7 +101,7 @@
             // 
             // button_minus
             // 
-            this.button_minus.Location = new System.Drawing.Point(668, 200);
+            this.button_minus.Location = new System.Drawing.Point(524, 260);
             this.button_minus.Name = "button_minus";
             this.button_minus.Size = new System.Drawing.Size(44, 44);
             this.button_minus.TabIndex = 11;
@@ -104,9 +111,9 @@
             // 
             // button_megse
             // 
-            this.button_megse.Location = new System.Drawing.Point(569, 342);
+            this.button_megse.Location = new System.Drawing.Point(236, 326);
             this.button_megse.Name = "button_megse";
-            this.button_megse.Size = new System.Drawing.Size(54, 45);
+            this.button_megse.Size = new System.Drawing.Size(156, 45);
             this.button_megse.TabIndex = 12;
             this.button_megse.Text = "Mégse";
             this.button_megse.UseVisualStyleBackColor = true;
@@ -114,19 +121,31 @@
             // 
             // button_mentes
             // 
-            this.button_mentes.Location = new System.Drawing.Point(645, 342);
+            this.button_mentes.Location = new System.Drawing.Point(398, 326);
             this.button_mentes.Name = "button_mentes";
-            this.button_mentes.Size = new System.Drawing.Size(54, 45);
+            this.button_mentes.Size = new System.Drawing.Size(170, 45);
             this.button_mentes.TabIndex = 13;
             this.button_mentes.Text = "Mentés";
             this.button_mentes.UseVisualStyleBackColor = true;
             this.button_mentes.Click += new System.EventHandler(this.button_mentes_Click);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(415, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(153, 141);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 14;
+            this.pictureBox1.TabStop = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(207)))), ((int)(((byte)(225)))), ((int)(((byte)(185)))));
+            this.ClientSize = new System.Drawing.Size(577, 379);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button_mentes);
             this.Controls.Add(this.button_megse);
             this.Controls.Add(this.button_minus);
@@ -137,7 +156,8 @@
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.textBox_kereses);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "KliensAlkalmazás";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -154,5 +174,6 @@
         private System.Windows.Forms.Button button_minus;
         private System.Windows.Forms.Button button_megse;
         private System.Windows.Forms.Button button_mentes;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
--- ApiSample/Form1.cs
+++ ApiSample/Form1.cs
@@ -9,17 +9,33 @@ using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Text.RegularExpressions;
 
 namespace ApiSample
 {
     public partial class Form1 : Form
     {
+        private bool ContainsNumber(string input)
+        {
+           
+            return Regex.IsMatch(input, @"\d");
+        }
+
+        private bool IsValidQuantity(string input)
+        {
+            
+            return Regex.IsMatch(input.Trim(), @"^-?\d+(\.\d+)?$");
+        }
         List<Termek> termeklista = new List<Termek>();
         public Form1()
 
         {
             InitializeComponent();
+            peldanyositas();
+        }
 
+        private void peldanyositas()
+        {
             var snaps = proxy.ProductsFindAll();
             for (int i = 0; i < snaps.Content.Count; i++)
             {
@@ -58,6 +74,15 @@ namespace ApiSample
         private void textBox_kereses_TextChanged_1(object sender, EventArgs e)
         {
             Szures();
+            if (ContainsNumber(textBox_kereses.Text))
+            {
+                MessageBox.Show("Kérjük csak betűket írjon be", "Érvénytelen bemenet", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+               
+            }
+
         }
 
         private void button_plus_Click_1(object sender, EventArgs e)
@@ -75,29 +100,55 @@ namespace ApiSample
         }
         private void button_mentes_Click(object sender, EventArgs e)
         {
-            var index = listBox1.SelectedIndex + 1;
-            var curproduct = termeklista[index];
-            var inv = proxy.ProductInventoryFind(curproduct.inventory_id).Content;
-            inv.QuantityOnHand = int.Parse(textBox_mennyiseg.Text);
-            var response = proxy.ProductInventoryUpdate(inv);
 
-            if (response != null)
+            int index = listBox1.SelectedIndex; 
+            if (index == -1) return;
+
+            try
             {
-                termeklista[index].keszlet = inv.QuantityOnHand;
+                var curproduct = termeklista[index];
+                var inv = proxy.ProductInventoryFind(curproduct.inventory_id).Content;
+                inv.QuantityOnHand = int.Parse(textBox_mennyiseg.Text);
+
+                var response = proxy.ProductInventoryUpdate(inv);
 
                 textBox_mennyiseg.Text = inv.QuantityOnHand.ToString();
-            };
+
+                if (!(response == null ))
+                {
+                    termeklista[index].keszlet = inv.QuantityOnHand;
+                    textBox_mennyiseg.Text = inv.QuantityOnHand.ToString();
+                    MessageBox.Show("Sikeres frissítés!", "Információ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    termeklista[index].keszlet = inv.QuantityOnHand;
+
+                    
+                    
+
+
+                }
+                else
+                {
+                    MessageBox.Show("Nem sikerült frissíteni az adatbázist.", "Hiba", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Kérjük, érvényes számot adjon meg!", "Érvénytelen bemenet", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
         private void listBox1_SelectedIndexChanged_1(object sender, EventArgs e)
         {
-            int index = ((ListBox)sender).SelectedIndex + 1;
-            textBox_mennyiseg.Text = (termeklista[index].keszlet).ToString();
-            this.Refresh();
+            int index = listBox1.SelectedIndex;
+            if (index == -1) return;
+           
+             textBox_mennyiseg.Text = (termeklista[index].keszlet).ToString();
+            
+           
         }
 
         private void button_megse_Click(object sender, EventArgs e)
         {
-            textBox_mennyiseg.Text = termeklista[listBox1.SelectedIndex + 1].keszlet.ToString();
+            textBox_mennyiseg.Text = termeklista[listBox1.SelectedIndex].keszlet.ToString();
 
         }
 
